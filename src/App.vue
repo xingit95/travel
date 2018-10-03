4e52adfaa06d9f289c37cdf518446d0fe5181774
xinzai <1213657724@qq.com>
2018-10-03 23:45:22 +0800

    'init'

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
@@ -12,6 +11,7 @@ export default {
 </script>
 
 <style>
+/*
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -19,5 +19,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+}*/
 </style>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 样式重置
+import './assets/styles/reset.css'
+// 高分屏的边框显示宽问题
+import './assets/styles/border.css'
+// 解决某些移动端click延迟问题
+import fastClick from 'fastclick'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
