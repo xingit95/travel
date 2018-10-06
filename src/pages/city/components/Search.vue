1c003b0e7557a13dae433199300b17e0e71b05f3
xinzai <1213657724@qq.com>
2018-10-06 21:25:28 +0800

    'searchfinished'

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,12 +1,69 @@
 <template>
-  <div class="search">
-    <input type="text" class="search-input" placeholder="输入城市名或拼音">
+  <div>
+    <div class="search">
+      <input v-model="keyword" type="text" class="search-input" placeholder="输入城市名或拼音">
+    </div>
+    <div class="search-content" ref="search" v-show="keyword">
+      <ul>
+        <li class="search-item border-bottom" v-for="item of List" :key="item.id">{{item.name}}</li>
+        <li class="search-item border-bottom" v-show="hasData">没有找到匹配数据</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import Bscroll from 'better-scroll'
 export default {
-  name: 'CitySearch'
+  name: 'CitySearch',
+  props: {
+    cities: Object
+  },
+  data () {
+    return {
+      keyword: '',
+      List: [],
+      timer: null
+    }
+  },
+  computed: {
+    hasData () {
+      return !this.List.length
+    }
+  },
+  watch: {
+    keyword () {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      if (!this.keyword) {
+        this.List = []
+        return
+      }
+      setTimeout(() => {
+        const result = []
+        for (const key in this.cities) {
+          this.cities[key].forEach((value) => {
+            const nameIndex = value.name.indexOf(this.keyword)
+            const spellIndex = value.spell.indexOf(this.keyword)
+            if (nameIndex > -1 || spellIndex > -1) {
+              if (nameIndex === 0 || spellIndex === 0) {
+                result.unshift(value)
+              } else {
+                result.push(value)
+              }
+            }
+          })
+        }
+        // console.log(result)
+        this.List = result
+        // console.log(this.List)
+      }, 100)
+    }
+  },
+  mounted () {
+    this.scroll = new Bscroll(this.$refs.search)
+  }
 }
 </script>
 
@@ -24,5 +81,20 @@ export default {
       line-height .62rem
       border-radius .06rem
       text-align center
+      color #666
+  .search-content
+    position absolute
+    top 1.58rem
+    bottom 0
+    left 0
+    right 0
+    z-index 1
+    overflow hidden
+    // margin 0 .5rem
+    background-color #eee
+    .search-item
+      line-height .62rem
+      padding-left .2rem
+      background-color #fff
       color #666
 </style>
